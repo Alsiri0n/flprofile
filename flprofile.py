91ebba106c7e9682c9e33c53d366d13f3b9f04cc
Ignatev Nikita <ignatev@medilon.ru>
2022-10-11 18:02:08 +0300

    Update route

--- flprofile.py
+++ flprofile.py
@@ -1,15 +1,15 @@
 """
 Login submodule for flask application
 """
-from flask import Blueprint,render_template,request,flash, session, redirect, url_for
+from flask import Blueprint,render_template
 
 flprofile = Blueprint('flprofile', __name__, template_folder='templates', static_folder='static')
 
 
-@flprofile.route('/profile/<username>')
+@flprofile.route('/<username>')
 def profile(username):
     """
     Index page for flprofile
     """
     return render_template('flprofile/profile.html', title='Твой профиль', username=username)
- 
+
