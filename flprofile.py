587bef9e12f17a1fdf8597e29e51625a6399c83c
Ignatev Nikita <ignatev@medilon.ru>
2022-10-12 11:12:56 +0300

    Add check at userLogged and 401 error

--- flprofile.py
+++ flprofile.py
@@ -1,7 +1,7 @@
 """
 Login submodule for flask application
 """
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, session, abort
 
 flprofile = Blueprint('flprofile', __name__, template_folder='templates', static_folder='static')
 
@@ -11,5 +11,7 @@ def profile(username):
     """
     Index page for flprofile
     """
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
     return render_template('flprofile/profile.html', title='Твой профиль', username=username)
 
